ae47c8a59f737773d92a5fff599227286f313654
twenty0ne <twenty0ne@qq.com>
2017-10-15 21:49:40 +0800

    update

--- Assets/Scripts/AI/HFSM/HFSM_Test.cs
+++ Assets/Scripts/AI/HFSM/HFSM_Test.cs
@@ -2,7 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// target：
+// normal state - walk, idle
+// walk state 
+// change to work state 
 public class HFSM_Test : MonoBehaviour 
 {
-	
+	private AI.IState rootState;
+
+	class NormalState : AI.State
+	{
+		public void OnUpdate()
+		{
+					
+		}
+	}
+
+	class WalkState : AI.State
+	{
+		
+	}
+
+	class RunState : AI.State
+	{
+		
+	}
 }
