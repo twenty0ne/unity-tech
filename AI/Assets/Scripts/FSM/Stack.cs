fd3dc86c4802d5683eb1e66264d5da085df2a9a4
twenty0ne <twenty0ne@qq.com>
2018-01-10 22:35:42 +0800

    update fsm

--- AI/Assets/Scripts/FSM/Stack.cs
+++ AI/Assets/Scripts/FSM/Stack.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 class Stack
 {
 	private Queue<State> states = new Queue<State> ();
+	private Dictionary<KeyValuePair<State, State>, System.Action> callbacks;
 
 	Stack(State start)
 	{
@@ -13,6 +14,12 @@ class Stack
 
 	private bool Call(State from, State to)
 	{
-		
+		KeyValuePair<State, State> kv = new KeyValuePair<State, State> (from, to);
+		if (callbacks.ContainsKey(kv))
+		{
+			callbacks [kv] ();
+			return true;
+		}
+		return false;
 	}
 }
