c572cc78fc638abe22be3750dfc2e82fb345b6ce
twenty0ne <twenty0ne@qq.com>
2018-10-17 09:25:08 +0800

    CS: update httpwebrequest

--- CS/Client/Assets/Https/Https.cs
+++ CS/Client/Assets/Https/Https.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -5,6 +6,7 @@ using System.Net;
 using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Https : MonoBehaviour
 {
@@ -12,7 +14,8 @@ public class Https : MonoBehaviour
 	{
 		Debug.Log("start request > ");
 		// StartCoroutine(Request());
-		StartCoroutine(RequestSSL());
+		RequestSSL();
+		// StartCoroutine(Request2());
 	}
 
 	// NOTE:
@@ -32,25 +35,55 @@ public class Https : MonoBehaviour
 		}
 	}
 
-	IEnumerator RequestSSL()
+	// https://docs.microsoft.com/en-us/dotnet/api/system.net.httpwebrequest.begingetresponse?redirectedfrom=MSDN&view=netframework-4.7.2#System_Net_HttpWebRequest_BeginGetResponse_System_AsyncCallback_System_Object_
+	private HttpWebRequest webRequest = null;
+	void RequestSSL()
 	{
 		ServicePointManager.ServerCertificateValidationCallback = TrustCertificate;
 
 		HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://127.0.0.1:14711");
-		HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		// HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+		request.BeginGetResponse(new AsyncCallback(FinishWebRequest), null);
+		webRequest = request;
+
+		//Stream dataStream = response.GetResponseStream();
+		//StreamReader reader = new StreamReader(dataStream);
+		//string responseFromServer = reader.ReadToEnd();
+
+		//Debug.Log("responseFromServer=" + responseFromServer);
+	}
+
+	void FinishWebRequest(IAsyncResult result)
+	{
+		HttpWebResponse response = (HttpWebResponse)webRequest.EndGetResponse(result);
 
 		Stream dataStream = response.GetResponseStream();
 		StreamReader reader = new StreamReader(dataStream);
 		string responseFromServer = reader.ReadToEnd();
 
 		Debug.Log("responseFromServer=" + responseFromServer);
-
-		yield return 0;
 	}
 
 	private static bool TrustCertificate(object sender, X509Certificate x509Certificate, X509Chain x509Chain, SslPolicyErrors sslPolicyErrors)
 	{
 		// all Certificates are accepted
 		return true;
+	}
+
+	// NOTE
+	// Unknow error
+	IEnumerator Request2()
+	{
+		UnityWebRequest www = new UnityWebRequest("https://127.0.0.1:14711");
+		yield return www.Send() ;
+
+		if (www.isError)
+		{
+			Debug.Log("request error > " + www.error);
+		}
+		else
+		{
+			Debug.Log("request return > " + www.downloadHandler.text);
+		}
 	}
 }
