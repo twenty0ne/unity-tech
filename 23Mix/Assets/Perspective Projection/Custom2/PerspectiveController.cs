761392acbe9e0be21f48037ccd96a75ed0f56c6c
twenty0ne <twenty0ne@qq.com>
2019-03-06 09:33:30 +0800

    [23Mix]add perspective 2

---
+++ 23Mix/Assets/Perspective Projection/Custom2/PerspectiveController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PerspectiveController : MonoBehaviour 
+{
+	public Camera camera; 
+
+	private void Start()
+	{
+
+	}
+	
+	private void Update () 
+	{
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			var cpos = camera.transform.position;
+			cpos.y += 0.1f;
+			camera.transform.position = cpos;
+		}
+		else if (Input.GetKey(KeyCode.DownArrow))
+		{
+			var cpos = camera.transform.position;
+			cpos.y -= 0.1f;
+			camera.transform.position = cpos;
+		}
+	}
+}
