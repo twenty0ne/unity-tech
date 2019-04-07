32a4ec03407a009a03ce1e4d7537c4f77ee08129
twenty0ne <twenty0ne@qq.com>
2019-04-07 09:53:35 +0800

    [ProBuilder]update Tower

--- ProBuilder/Assets/Tower/Tower.cs
+++ ProBuilder/Assets/Tower/Tower.cs
@@ -4,5 +4,37 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour 
 {
+	[Range(10, 20)]
+	public float rotateSpeed;
 
+	public GameObject objStairCase;
+	public GameObject objStairCorner;
+
+	public Vector3 stairStartPos;
+
+	private float stairHeight;
+
+	private void Start() 
+	{
+		Debug.Assert(objStairCase != null, "CHECK");
+		stairHeight = objStairCase.GetComponent<Renderer>().bounds.size.y;
+		Debug.Log("xx-- stairHeight > " + stairHeight);
+
+		InitStairs();
+	}
+
+	private void Update() 
+	{
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.RotateAround(transform.position, Vector3.up, rotateSpeed * Time.deltaTime);
+			// transform.RotateAround(Vector3.up, rotateSpeed * Time.deltaTime);
+			// transform.Rotate(0, rotateSpeed * Time.deltaTime, 0, Space.Self);
+		}
+	}
+
+	private void InitStairs()
+	{
+
+	}
 }
--- ProBuilder/README.md
+++ ProBuilder/README.md
@@ -1,4 +1,11 @@
 # NOTE
 ## 操作  
-1.Shift  
-扩展
+
+**1.扩展**  
+按住 Shift 移动选择的 点/线/面  
+
+**2.设置 Povit**  
+如下 2 种方式  
+ProBuilder > Object > Center Povit  
+ProBuilder > Geometry > Set Povit To Selection  
+
