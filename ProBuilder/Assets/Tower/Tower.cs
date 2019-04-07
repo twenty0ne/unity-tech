2f8c632972afb7a9d20143915750c807e25de07a
twenty0ne <twenty0ne@qq.com>
2019-04-07 10:13:10 +0800

    [ProBuilder]update Tower

--- ProBuilder/Assets/Tower/Tower.cs
+++ ProBuilder/Assets/Tower/Tower.cs
@@ -10,15 +10,15 @@ public class Tower : MonoBehaviour
 	public GameObject objStairCase;
 	public GameObject objStairCorner;
 
-	public Vector3 stairStartPos;
+	public Vector3 posStairStart;
 
-	private float stairHeight;
+	private Vector3 stairSize;
 
 	private void Start() 
 	{
 		Debug.Assert(objStairCase != null, "CHECK");
-		stairHeight = objStairCase.GetComponent<Renderer>().bounds.size.y;
-		Debug.Log("xx-- stairHeight > " + stairHeight);
+		stairSize = objStairCase.GetComponent<Renderer>().bounds.size;
+		Debug.Log("xx-- stairSize > " + stairSize);
 
 		InitStairs();
 	}
@@ -35,6 +35,12 @@ public class Tower : MonoBehaviour
 
 	private void InitStairs()
 	{
-
+		for (int i = 0; i < 10; ++i)
+		{
+			GameObject stairCase = Instantiate(objStairCase);
+			stairCase.transform.SetParent(transform);
+			stairCase.transform.position = posStairStart + 
+					new Vector3(i * stairSize.x, i * stairSize.y, 0);
+		}
 	}
 }
