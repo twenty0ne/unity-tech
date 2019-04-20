03051ebbb024370783148bf5d4612b52aeb219e4
twenty0ne <twenty0ne@qq.com>
2019-04-20 09:02:59 +0800

    UI

--- UI/Assets/Scripts/UITest/MenuMain.cs
+++ UI/Assets/Scripts/UITest/MenuMain.cs
@@ -6,18 +6,6 @@ public class MenuMain : UIMenu
 {
 	private void Start()
 	{
-		StartCoroutine(InitWidgets());
-	}
-
-	private IEnumerator InitWidgets()
-	{
-		GameObject objWidget = AssetManager.LoadGameObject(AssetDefine.PATH_PREFAB_UI_WIDGET	+ "WidgetMainIcon");
-		Debug.Assert(objWidget != null, "CHECK");
-
-		for (int i = 0; i < )
-
-
-		yield return null;
 	}
 
 	public void OnClickWidgetMainIcon(WidgetMainIcon widget)
