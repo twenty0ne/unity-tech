7e58c54b1be1a8755f83efcff505456d1c1e8f75
twenty0ne <twenty0ne@qq.com>
2019-07-13 13:08:55 +0800

    update

--- UI/Assets/TestListView/TestListView.cs
+++ UI/Assets/TestListView/TestListView.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestListView : MonoBehaviour
 {
@@ -22,6 +23,8 @@ public class TestListView : MonoBehaviour
 
 		// test long list
 		listView.itemCount = 20;
+
+		// GetComponent<ScrollRect>
 	}
 
 	private void OnItemClick(TestListItem item)
