1a6332b606572103858027d59806c4071b68ceba
twenty0ne <twenty0ne@qq.com>
2019-07-15 19:57:52 +0800

    [UI]update

--- UI/Assets/Scripts/XGUI/UIPageView.cs
+++ UI/Assets/Scripts/XGUI/UIPageView.cs
@@ -1,12 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIPageView : UIWidget
 {
 	public UIPageViewIndicator indicator;
+	public ScrollRect scrollRect;
+
+	private void Awake()
+	{
+		scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
+	}
 
 	public override void AddChild(UIWidget widget)
+	{
+		widget.transform.SetParent(transform, false);
+	}
+
+	private void OnScrollValueChanged(Vector2 vec2)
 	{
 		
 	}
--- UI/Assets/TestPageView/TestPageView.cs
+++ UI/Assets/TestPageView/TestPageView.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class TestPageView : UIPanel
 {
 	public UIPageView pageView;
+	public GameObject itemPrefab;
 
 	private void Awake() 
 	{
-		// pageView.AddChild()
+		pageView = GetComponent<UIPageView>();
+
+		for (int i = 0; i < 10; ++i)
+		{
+			GameObject obj = Instantiate(itemPrefab);
+			TestPageViewItem item = obj.GetComponent<TestPageViewItem>();
+
+			pageView.AddChild(item);
+		}
 	}
 
 	void Start()
 	{
-
+		
 	}
 
 	public void OnClick()
