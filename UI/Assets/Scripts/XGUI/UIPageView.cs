a74751bc979cb255d6bd2003a3b6cbe52bbdbe52
twenty0ne <twenty0ne@qq.com>
2019-07-17 06:48:35 +0800

    update

--- UI/Assets/Scripts/XGUI/UIPageView.cs
+++ UI/Assets/Scripts/XGUI/UIPageView.cs
@@ -2,24 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class UIPageView : UIWidget
+public class UIPageView : UIWidget, IPointerUpHandler
 {
 	public UIPageViewIndicator indicator;
 	public ScrollRect scrollRect;
 
+	private RectTransform contentRT;
+
+	// public List<UIWidget> children = new List<UIWidget>();
+
 	private void Awake()
 	{
+		contentRT = GetComponent<RectTransform>();
+
 		scrollRect.onValueChanged.AddListener(OnScrollValueChanged);
 	}
 
 	public override void AddChild(UIWidget widget)
 	{
 		widget.transform.SetParent(transform, false);
+		children.Add(widget);
 	}
 
-	private void OnScrollValueChanged(Vector2 vec2)
+	private void OnScrollValueChanged(Vector2 val)
 	{
-		Debug.Log("xx-- UIPageView.OnScrollValueChanged > " + (vec2.x * 1080f));
+		// Debug.Log("xx-- UIPageView.OnScrollValueChanged > " + vec2.x + " - " + vec2.x * contentRT.sizeDelta.x);
+		float vx = val.x;
+
+	}
+
+	// private void OnMouseUp() 
+	// {
+	// 	Debug.Log("xx-- OnMouseUp");
+	// }
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		Debug.Log("xx-- OnPointerUp");
 	}
 }
