d0327d912d969d06e84afef8ab8ca0705d34165f
twenty0ne <twenty0ne@qq.com>
2019-08-29 13:21:45 +0800

    [UI]update

---
+++ UI/Assets/Scripts/UITest/MenuLoading.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuLoading : UIMenu
+{
+	
+
+	private void Start()
+	{
+
+	}
+
+	private void Update() 
+	{
+		
+	}
+}
