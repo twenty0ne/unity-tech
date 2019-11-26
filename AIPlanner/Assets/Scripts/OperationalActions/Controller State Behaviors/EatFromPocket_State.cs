77812a0bc42eedadb97d3e857d0c3940ef013e54
twenty0ne <twenty0ne@qq.com>
2019-11-26 08:55:29 +0800

    [AIPlanner]add

---
+++ AIPlanner/Assets/Scripts/OperationalActions/Controller State Behaviors/EatFromPocket_State.cs
@@ -0,0 +1,16 @@
+﻿#if PLANNER_ACTIONS_GENERATED
+using System;
+using UnityEngine;
+using Workaholic;
+
+public class EatFromPocket_State : StateMachineBehaviour
+{
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    public override void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        var otto = animator.gameObject.GetComponentInChildren<Otto>();
+        var action = (ConsumeAction)otto.CurrentOperationalAction;
+        action.AnimationComplete = true;
+    }
+}
+#endif
---
+++ AIPlanner/Assets/Scripts/OperationalActions/WorkAction.cs
@@ -0,0 +1,45 @@
+﻿#if PLANNER_ACTIONS_GENERATED
+using System;
+using AI.Planner.Domains;
+using AI.Planner.Domains.Enums;
+using Unity.AI.Planner.Agent;
+using Unity.AI.Planner.DomainLanguage.TraitBased;
+using Workaholic;
+using UnityEngine;
+using Time = UnityEngine.Time;
+
+public class WorkAction : OttoAction
+{
+    static readonly int k_Work = Animator.StringToHash("Work");
+    float m_Duration;
+    const float k_ExitTime = 0.667f;
+    static readonly int s_ContinueWork = Animator.StringToHash("ContinueWork");
+
+    public override void BeginExecution(StateData state, ActionKey action, Otto actor)
+    {
+        base.BeginExecution(state, action, actor);
+
+        m_StartTime = Time.time;
+
+        m_Duration = state.GetTraitOnObjectAtIndex<Duration>(action[2]).Time;
+
+        m_Animator.SetTrigger(k_Work);
+        m_Animator.SetBool(s_ContinueWork, true);
+    }
+
+    public override void ContinueExecution(StateData state, ActionKey action, Otto actor)
+    {
+        base.ContinueExecution(state, action, actor);
+
+        if (Time.time - m_StartTime >= m_Duration - k_ExitTime)
+            m_Animator.SetBool(s_ContinueWork, false);
+    }
+
+    public override OperationalActionStatus Status(StateData state, ActionKey action, Otto actor)
+    {
+        return Time.time - m_StartTime >= m_Duration ?
+            OperationalActionStatus.Completed :
+            OperationalActionStatus.InProgress;
+    }
+}
+#endif
